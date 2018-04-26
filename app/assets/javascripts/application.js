55bdcc0c939d7e4858de9d6dde03bf5e71ceb061
Khalil Gharbaoui <kaygeee@gmail.com>
2018-04-26 18:00:04 +0200

    Add more tests, validations, configuration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+//= require rails_address_autocomplete
+
+
+// This function is call when Google Maps is loaded
+window.googleMapInitialize = function(){
+    // Advanced usage with google options
+    $('.rails-address-autocomplete').rails_address_autocomplete({
+        // type: ['restaurant'],
+        componentRestrictions: { country: 'ch' }
+    });
+};
+
+// $('.rails-address-autocomplete').on('change:address:data_changed', function(event, data){
+// 	console.log("LOG: " + data);
+// });
