3a41ac808d978296b342c82c8d22da2e60703445
Khalil Gharbaoui <kaygeee@gmail.com>
2018-04-26 19:11:38 +0200

    Add moving_date validation for format, presence, nil

--- app/models/moving_inquiry.rb
+++ app/models/moving_inquiry.rb
@@ -1,6 +1,6 @@
 class MovingInquiry < ApplicationRecord
   validates :locale, :is_moving_request, :is_cleaning_request,
-            :client_salutation, :client_first_name, :client_last_name, :client_email, :client_mobile, :client_postal_code, :client_city, :moving_postal_code, :moving_city, :client_property_size, exclusion: { in: [nil] }
+            :client_salutation, :client_first_name, :client_last_name, :client_email, :client_mobile, :client_postal_code, :client_city, :moving_postal_code, :moving_city, :moving_date, :client_property_size, exclusion: { in: [nil] }
 
   validates :locale, :client_salutation, :client_first_name,
             :client_last_name, :client_postal_code,:moving_postal_code, :client_city, :moving_city, :client_property_size, presence: true
@@ -20,4 +20,6 @@ class MovingInquiry < ApplicationRecord
   validates :client_mobile, phone: {possible: true, allow_blank: false, types: [:fixed_or_mobile,:fixed_line, :mobile]}
 
   validates :client_postal_code, :moving_postal_code, zipcode: { country_code: :ch }
+  validates_format_of :moving_date, with: /\d{4}\-\d{2}\-\d{2}/, message: "^Date must be in the following format: yyyy-mm-yy"
+
 end
