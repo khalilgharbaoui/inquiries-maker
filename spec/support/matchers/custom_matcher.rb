7ddacfdb92243e11e304a48d479a9495d14bb216
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-05-10 13:49:39 +0200

    Add custom matcher, specs & validation for client_moving_street
    The custom rspec matcher makes sure there is a digit inside a string.

--- spec/support/matchers/custom_matcher.rb
+++ spec/support/matchers/custom_matcher.rb
@@ -12,3 +12,15 @@ RSpec::Matchers.define :parse_to_http_url do |expected|
 		expect(uri.scheme).to start_with("http")
 	end
 end
+
+RSpec::Matchers.define :have_digits do |expected|
+	match do |actual|
+		def has_digits?(str)
+  		str.count("0-9") > 0 && str.match(/\d+/)
+		end
+		expect(actual).to_not be_nil
+		expect(actual.class).to equal(String)
+		expect(has_digits?(actual)).not_to be_nil
+		expect(has_digits?(actual)).not_to be_falsy
+	end
+end
