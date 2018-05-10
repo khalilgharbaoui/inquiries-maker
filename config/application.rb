d47d384669e7a2451f20e851a64a63400cdc6797
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-05-11 00:39:05 +0200

    [WIP] Fix multi language form

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module InquiriesMaker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # Whitelist locales available for the application
+    # config.i18n.available_locales = [:de,:en,:fr]
+    # config.i18n.default_locale = :de
   end
 end
