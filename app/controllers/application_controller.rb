544739a622d1877053cff06f67af5845c6504dfb
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-05-12 02:58:47 +0200

    Fix form autocomplete & add language switcher and dynamic form field
    - lang_switcher helper presents 3 choices and sets them on click.
    - locale_field helper either sets a hidden_field with locale in it or an editabele field with original value in it.
    this depends on inquiry.presisted?
    - configured application available_locales to hold [:de,:fr,:en]
    - configured application default_locale to :de
    - add routes for languages only for the :new action
    - cleand up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,39 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale, only: [:new, :create]
-
   before_action :set_locale
-
-def set_locale
-  I18n.locale = params[:locale] || extract_locale_from_accept_language_header
-end
-
-def default_url_options
-  { locale: I18n.locale }
-end
-
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_from_params_or_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
   # if current_admin_user
   #    I18n.locale = :en #or better: default_locale
   # else
   #    #Your logic
   # end
-
+  #
   # if current_user.is_admin?
   #    I18n.locale = :en #or better: default_locale
   # else
   #    #Your logic
   # end
-
+  #
   # if self.kind_of? ActiveAdmin::BaseController
   #    I18n.locale = :en
   # else
   #  #Your logic
   # end
-
-  #TODO: COVER MORE LANGUAGE CODES!
-  def extract_locale_from_accept_language_header
-    if %w[de fr en].include?(request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first)
-      return request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    elsif %w[de fr en].include?(!request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first)
-      return 'en'
-    else
-      return I18n.default_locale
-    end
+  private
+  def extract_from_params_or_language_header
+    extract_from_params || extract_from_language_header
+  end
+  def extract_from_params
+    params[:locale] if %w[de fr en].include?(params[:locale])
+  end
+  def extract_from_language_header
+    header = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    header if %w[de fr en].include?(header)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  # found here: https://dhampik.com/blog/rails-routes-tricks-with-locales
+  def lang_switcher
+    content_tag(:ul, class: 'lang-switcher clearfix') do
+      I18n.available_locales.each do |loc|
+        concat content_tag(:li, (link_to loc, params.permit(:locale).merge(locale: loc)), class: (I18n.locale == loc ? "active" : ""))
+      end
+    end
+  end
+
+  def locale_field(inquiry, f)
+    if inquiry.persisted?
+      content_tag(:div, class: "field") do
+        f.label :locale
+        f.text_field :locale
+      end
+    else
+      content_tag(:div, class: "field") do
+        f.hidden_field :locale, value: params[:locale] ||= I18n.locale
+      end
+    end
+  end
 end
