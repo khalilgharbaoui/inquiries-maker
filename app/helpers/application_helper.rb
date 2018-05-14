4b4ebfa8849d0151f74e1273ae12d6c15f62e971
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-05-14 04:38:05 +0200

    Improve views and configure device
    - Fixed both language switchers with bootstrap
    - Fixed index page with bootstrap
    - configured device routes
    - restricted acces to inquiries
    - redirected sign ups

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,20 @@ module ApplicationHelper
 
 
   def general_lang_switcher
-    content_tag(:div, class: 'lang-switcher clearfix') do
+    content_tag(:ul, class: 'general-lang-switcher navbar-nav mr-auto') do
       I18n.available_locales.each do |loc|
-        concat content_tag(:span, (link_to loc, params.permit(:locale).merge(locale: loc)), class: (I18n.locale == loc ? "active" : "")) + content_tag(:span,"  ")
+        concat content_tag(:li, (link_to loc.upcase, params.permit(:locale).merge(locale: loc), class: "nav-link"), class: "nav-item #{(I18n.locale == loc ? "active" : "")}").html_safe
       end
     end
   end
 
   def form_lang_switcher(action)
-    content_tag(:div, class: 'lang-switcher clearfix') do
-      I18n.available_locales.each do |loc|
-        concat content_tag(:span, (link_to loc,
-        url_for(action: :"#{action}", locale: loc)), class: (I18n.locale == loc ? "active" : "")) + content_tag(:span,"  ")
+    content_tag(:nav, :"aria-label" => 'language switch') do
+     content_tag(:ul, class: 'pagination pagination-sm justify-content-end') do
+        I18n.available_locales.each do |loc|
+          concat content_tag(:li, (link_to loc.upcase,
+          url_for(action: :"#{action}", locale: loc), class: "page-link"), class: "page-item #{(I18n.locale == loc ? "active" : "")}").html_safe
+        end
       end
     end
   end
