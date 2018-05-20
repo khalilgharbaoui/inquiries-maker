3583df7ee0e0dd56cf8f2eb3274aec1682249ad8
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-05-20 05:39:11 +0200

    Add Sidekiq with Redis for ActiveJob
    Also added and commented out Sneakers gem and configuration.
    Will be needing this in the future for use with RabitMQ instead of Sidekiq.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,9 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
+# For rabitMQ
 require_relative 'config/application'
+# require 'sneakers/tasks'
 
 Rails.application.load_tasks
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,18 +49,19 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: Rails.application.secrets.domain_name,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: Rails.application.secrets.email_provider_username,
+  #   password: Rails.application.secrets.email_provider_password
+  # }
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
