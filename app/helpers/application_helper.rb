abec92d53d60ebd7d95de761a60653a204fd05d2
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-02 07:45:56 +0200

    Fix form and error messages, error massages explanation and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,37 @@ module ApplicationHelper
         |key, value| [I18n.t("activerecord.enums.#{klass.downcase}.#{enum}.#{key}").humanize, key]
     }
   end
+
+#   def errors_for(object)
+#       if object.errors.any?
+#           content_tag(:div, class: "card border-danger") do
+#               concat(content_tag(:div, class: "card-header bg-danger text-white") do
+#                   concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
+#               end)
+#               concat(content_tag(:div, class: "card-body") do
+#                   concat(content_tag(:ul, class: 'mb-0') do
+#                       object.errors.full_messages.each do |msg|
+#                           concat content_tag(:li, msg)
+#                       end
+#                   end)
+#               end)
+#           end
+#       end
+#   end
+# end
+
+def errors_for(object)
+    if object.errors.any?
+        content_tag(:div, class: "card border-danger custom_error_explanation") do
+            concat(content_tag(:div, class: "card-header bg-danger text-white") do
+                concat "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:"
+            end)
+            concat(content_tag(:ul, class: 'mb-0 list-group list-group-flush') do
+                object.errors.full_messages.each do |msg|
+                    concat content_tag(:li, msg, class: 'list-group-item')
+                end
+            end)
+        end
+    end
+end
 end
