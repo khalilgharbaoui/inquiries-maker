e19de7c2982628cfec185fc7566e17f64b6192e8
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-23 13:40:58 +0200

    Add view partial for received inquiry response

--- app/models/received_inquiry_response.rb
+++ app/models/received_inquiry_response.rb
@@ -2,5 +2,10 @@ class ReceivedInquiryResponse < ApplicationRecord
   belongs_to :moving_iquiry, optional: true
   belongs_to :cleaning_iquiry, optional: true
   belongs_to :combined_iquiry, optional: true
-  # after_create :schedule_client_emails
+  before_save ResponseBodyWrapper.new
+  after_create :schedule_client_emails(self)
+
+  def schedule_client_emails
+    ClientMailer.client(self).deliver_later
+  end
 end
