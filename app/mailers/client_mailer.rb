e6a214b9be4ca72db61dc7cc4ec6ff40d7682b98
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-29 08:11:00 +0200

    Make the client mailer generic, it can take any inquiry now.

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,11 +1,25 @@
+# frozen_string_literal: true
+
 class ClientMailer < ApplicationMailer
+  helper :markdown
+  def client(response_body, inquiry)
+    @continue_url = response_body[:continue_url]
+    @inquiry = inquiry
+    @inquiry_name = t(".inquiry.#{inquiry.class.name.underscore.downcase}")
+    @client_salutation = t("#{@inquiry.class.name.pluralize.underscore}.form.client_salutations.#{inquiry.client_salutation}")
+    sender = Rails.application.credentials.dig(Rails.env.to_sym, :sender_email)
 
-  def client(moving_inquiry_id)
-    moving_inquiry = MovingInquiry.find(moving_inquiry_id)
-    @client = moving_inquiry
-
-    mail to: "#{moving_inquiry.client_last_name} <#{moving_inquiry.client_email}>",
-      from: "Umzug Offerte Schweiz <info@umzug-offetre-schweiz.com>",
-      subject: "#{moving_inquiry.client_last_name}, Thank you for your request!"
+    mail to: "#{inquiry.client_last_name} #{inquiry.client_first_name}\
+    <#{inquiry.client_email}>",
+     'Reply-To': sender,
+     'Importance': 'High',
+     'Sensitivity': 'private',
+     'Language': @inquiry.locale,
+     'Sender': sender,
+     from: sender,
+     subject: t('.complete_your_inquiry.subject', inquiry: @inquiry_name) do |format|
+       format.html
+       format.text
+    end
   end
 end
