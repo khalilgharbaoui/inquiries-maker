b27953580cca97696b8bb2871f91fd33a94a238a
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-06-29 07:07:13 +0200

    Add email_previews route

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
-  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
-    root to: "moving_inquiries#new"
+Rails.application.routes.draw do
+  scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
+    mount RailsEmailPreview::Engine, at: 'emails'
+    root to: 'moving_inquiries#new'
     devise_for :users, controllers: {
-          sessions: 'users/sessions',
-          registrations: "users/registrations"
-        }
-    post "moving_inquiries/new", to: "moving_inquiries#create", as: 'create_moving_inquiry'
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+    post 'moving_inquiries/new', to: 'moving_inquiries#create', as: 'create_moving_inquiry'
     # post "cleaning_inquiries/new", to: "cleaning_inquiries#create", as: 'create_cleaning_inquiry'
     # post "combined_inquiries/new", to: "combined_inquiries#create", as: 'create_combined_inquiry'
     resources :moving_inquiries, except: [:create]
     # resources :cleaning_inquiries, except: [:create]
     # resources :combined_inquiries, except: [:create]
   end
-  get "*path", to: redirect("/#{I18n.locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
-  get '*locale/*action/*path', to: redirect("/#{I18n.locale}/%{action}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
-  get '/*locale/*action/*path', to: redirect("/#{I18n.locale}/%{action}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
-
+  get '*path', to: redirect("/#{I18n.locale}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
+  get '*locale/*action/*path', to: redirect("/#{I18n.locale}/%{action}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
+  get '/*locale/*action/*path', to: redirect("/#{I18n.locale}/%{action}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
 end
