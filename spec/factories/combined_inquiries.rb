914c94c860d509d1f41010b82ae101c7fbd3a8f7
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-01 05:07:12 +0200

    Fix all inquiries Factories

--- spec/factories/combined_inquiries.rb
+++ spec/factories/combined_inquiries.rb
@@ -1,20 +1,23 @@
 FactoryBot.define do
   factory :combined_inquiry do
-    partner_token "MyString"
-    locale "MyString"
-    client_first_name "MyString"
-    client_last_name "MyString"
-    client_salutation "MyString"
-    client_email ""
-    client_mobile "MyString"
-    client_street_and_number "MyString"
-    client_postal_code "MyString"
-    client_property_size "MyString"
-    is_moving_request false
-    is_cleaning_request false
-    moving_street_and_number "MyString"
-    moving_postal_code "MyString"
-    moving_date "2018-04-22"
-    cleaning_date "2018-04-22"
+    #TODO: FIX THIS:
+    locale { Faker::SwissLocals.locale }
+    is_moving_request true
+    is_cleaning_request true
+    # client_salutation { Faker::SwissLocals.random_salutations }
+    client_salutation { Faker::SwissLocals.salutations("de") } # de en fr
+    client_first_name { Faker::Name.first_name }
+    client_last_name { Faker::Name.last_name }
+    client_email { Faker::Internet.email }
+    client_mobile { Faker::PhoneNumber.cell_phone }
+    client_street_and_number { Faker::Address.street_name + " " + Faker::Address.building_number}
+    client_postal_code { Faker::Address.postcode }
+    client_city { Faker::Address.city }
+    client_property_size ["size_1", "size_2", "size_3", "size_4", "size_5", "size_6", "size_7", "size_8"].shuffle.first
+    moving_street_and_number { Faker::Address.street_name + " " + Faker::Address.building_number}
+    moving_postal_code { Faker::Address.postcode }
+    moving_city { Faker::Address.city }
+    moving_date {Faker::Date.forward(days = 365)}
+    cleaning_date {Faker::Date.forward(days = 368)}
   end
 end
--- spec/factories/moving_inquiries.rb
+++ spec/factories/moving_inquiries.rb
@@ -2,22 +2,18 @@ FactoryBot.define do
   factory :moving_inquiry do
     #TODO: FIX THIS:
     locale { Faker::SwissLocals.locale }
-    # client_salutation { Faker::SwissLocals.random_salutations }
-    client_salutation { Faker::SwissLocals.salutations("de") } # de en fr
     is_moving_request true
     is_cleaning_request false
-
+    # client_salutation { Faker::SwissLocals.random_salutations }
+    client_salutation { Faker::SwissLocals.salutations("de") } # de en fr
     client_first_name { Faker::Name.first_name }
     client_last_name { Faker::Name.last_name }
-
     client_email { Faker::Internet.email }
     client_mobile { Faker::PhoneNumber.cell_phone }
     client_street_and_number { Faker::Address.street_name + " " + Faker::Address.building_number}
-
     client_postal_code { Faker::Address.postcode }
     client_city { Faker::Address.city }
     client_property_size ["size_1", "size_2", "size_3", "size_4", "size_5", "size_6", "size_7", "size_8"].shuffle.first
-
     moving_street_and_number { Faker::Address.street_name + " " + Faker::Address.building_number}
     moving_postal_code { Faker::Address.postcode }
     moving_city { Faker::Address.city }
