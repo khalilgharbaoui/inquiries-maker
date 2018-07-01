434429b2843b7ca6245604af1d3c84ec75d5b8e3
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-01 11:18:48 +0200

    Fix datetime helper

--- app/helpers/inquiry_datetime_helper.rb
+++ app/helpers/inquiry_datetime_helper.rb
@@ -1,7 +1,7 @@
 module InquiryDatetimeHelper
   def inquiry_conclusion_date(inquiry)
     inquiry_type = inquiry.class.name.underscore.split("_")[0]
-    conclusion_date = inquiry.send :"#{inquiry_type}_date"
+    inquiry_type == 'combined' ? (conclusion_date = inquiry.send :"moving_date") : (conclusion_date = inquiry.send :"#{inquiry_type}_date")
     return conclusion_date.to_s(:swiss_date_format)
   end
   def inquiry_creation_date(inquiry)
