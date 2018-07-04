d933881a6848a4119216cafa5b48793558c92ae7
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-04 04:01:09 +0200

    Fix first visit language detection, cleanup language switcher, add user_locale
    Add better detection with rails_locale_detection gem.
    Remove the need for setting the locale inside the language switche helper.
    Add method for detection current_user locale if user has a locale.
    Tested this in the browser, also working in an iframe on diffrent domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   include LocaleFromBrowserConcern
 
-  # TODO: Add method to set admin locale to :en
-  # def default_url_options
-  #   { locale: I18n.locale }
-  # end
-  # if current_admin_user
-  #    I18n.locale = :en #or better: default_locale
-  # else
-  #    #Your logic
-  # end
-  #
-  # if current_user.is_admin?
-  #    I18n.locale = :en #or better: default_locale
-  # else
-  #    #Your logic
-  # end
+  def user_locale
+    current_user.locale if current_user
+  end
 end
