c606c794084631d168b7a275cd759b6753863cc5
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-13 00:45:41 +0200

    Enhance ParenThankYouPageUrl Module
    Now i generate the parent url from the origin field.
    Diffrent pages have diffrent origin url.
    It all come here together dynamically.
    Also i dont grab the inquiry type from the caller anymore.
    Instead im using the params[:controller] which was availeble to us all along.

--- app/controllers/concerns/parent_thank_you_page_url_maker.rb
+++ app/controllers/concerns/parent_thank_you_page_url_maker.rb
@@ -1,10 +1,9 @@
 module ParentThankYouPageUrlMaker
-  def parent_thank_you_page
-    locale = I18n.locale
-    url = Rails.application.credentials.dig(Rails.env.to_sym, :uos_url)
-    caller_class_name = caller.first.split("/").pop.split(".rb").first
-    inquiry_type = caller_class_name.split("_").first
-    thank_you_path = "thank_you_#{inquiry_type}"
-    return "#{url}/#{locale}/#{t(thank_you_path)}/"
+  def parent_thank_you_page_url
+      locale = I18n.locale
+      inquiry_type = params[:controller].split("_").first
+      url = instance_variable_get("@#{inquiry_type}_inquiry").origin
+      thank_you_path = "thank_you_#{inquiry_type}"
+      return "#{url}/#{locale}/#{t(thank_you_path)}/"
   end
 end
