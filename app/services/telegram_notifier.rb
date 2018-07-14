ae5a0f1e13d0da2b5b2d10e553557b40e26b762b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-14 09:59:26 +0200

    [WIP] telegram notifier

--- app/services/telegram_notifier.rb
+++ app/services/telegram_notifier.rb
@@ -1,6 +1,6 @@
 module TelegramNotifier
-  def notify(message)
-    bot = Telebot::Bot.new(Rails.application.credentials.dig(Rails.env.to_sym,:telegram_token))
+  def self.notify(message)
+    bot = Telebot::Client.new(Rails.application.credentials.dig(Rails.env.to_sym,:telegram_token))
     uid = Rails.application.credentials.dig(Rails.env.to_sym,:telegram_user_id)
     bot.send_message(chat_id: uid, text: "#{message}", disable_web_page_preview: true)
   end
