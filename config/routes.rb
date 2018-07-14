d48e9e7499e0391fe3ff080a598c725eeac866e2
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-14 07:16:13 +0200

    Create routes that point to the reschedule action

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,16 @@ Rails.application.routes.draw do
     post 'moving_inquiries/new', to: 'moving_inquiries#create', as: 'create_moving_inquiry'
     post 'cleaning_inquiries/new', to: 'cleaning_inquiries#create', as: 'create_cleaning_inquiry'
     post 'combined_inquiries/new', to: 'combined_inquiries#create', as: 'create_combined_inquiry'
-    resources :moving_inquiries, except: [:create]
-    resources :cleaning_inquiries, except: [:create]
-    resources :combined_inquiries, except: [:create]
+    # get 'reschedule_inquiry/:id', to: 'moving_inquiries#reschedule_inquiry', as: 'reschedule_inquiry'
+    resources :moving_inquiries, except: [:create] do
+      get 'reschedule_inquiry/:id', to: "moving_inquiries#reschedule_inquiry", on: :collection
+    end
+    resources :cleaning_inquiries, except: [:create] do
+      get 'reschedule_inquiry/:id', to: "cleaning_inquiries#reschedule_inquiry", on: :collection
+    end
+    resources :combined_inquiries, except: [:create] do
+      get 'reschedule_inquiry/:id', to: "combined_inquiries#reschedule_inquiry", on: :collection
+    end
   end
   get '*path', to: redirect("/#{I18n.locale}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
   get '*locale/*action/*path', to: redirect("/#{I18n.locale}/%{action}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
