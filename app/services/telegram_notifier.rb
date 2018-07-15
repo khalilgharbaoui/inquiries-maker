998c151869de4411488bf3b3a5c3bcf841a0b014
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-07-15 09:14:46 +0200

    Fix TelegramNotifier

--- app/services/telegram_notifier.rb
+++ app/services/telegram_notifier.rb
@@ -1,7 +1,19 @@
-module TelegramNotifier
-  def self.notify(message)
-    bot = Telebot::Client.new(Rails.application.credentials.dig(Rails.env.to_sym,:telegram_token))
-    uid = Rails.application.credentials.dig(Rails.env.to_sym,:telegram_user_id)
-    bot.send_message(chat_id: uid, text: "#{message}", disable_web_page_preview: true)
+class TelegramNotifier
+
+  def initialize(object)
+    @message = "*#{object.class.name}:*\n```json\n#{JSON.pretty_generate(object.as_json)}\n```"
+    @bot = Telebot::Client.new(Rails.application.credentials.dig(Rails.env.to_sym,:telegram_token))
+    @uid = Rails.application.credentials.dig(Rails.env.to_sym,:telegram_user_id)
+    send_message
+  end
+
+  private
+
+  def send_message
+    @bot.send_message(
+      chat_id: @uid,
+      text: @message,
+      parse_mode: "Markdown"
+    )
   end
 end
