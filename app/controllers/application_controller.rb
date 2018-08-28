08bb50130d670a7608712b40eccda4be3e7dd02e
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-28 12:09:43 +0200

    Comment out user_locale method we are not using it at the moment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   include LocaleFromBrowser
 
-  def user_locale
-    current_user.locale if current_user
-  end
+  # def user_locale
+  #   current_user.locale if current_user
+  # end
 end
