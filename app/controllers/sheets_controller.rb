3c65e91f3954b029a69e9057eb286f9e9f87183b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-08-28 13:35:48 +0200

    Refactor sheets controller

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -2,16 +2,22 @@ class SheetsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @inquiries = if params[:quarter]
-      ReceivedInquiryResponse.order("id DESC").where('quarter = ?', params[:quarter])
-    else
-      ReceivedInquiryResponse.order("id DESC")
-    end
+    @inquiries = received_inquiry_responces(params[:quarter])
     response.headers['Content-Disposition'] = 'inline'
     render xlsx: @inquiries, disposition: 'inline',
            filename: "uos-leads-list#{'-' + params[:quarter].tr(' ','-') if params[:quarter]}.xlsx",
            xlsx_created_at: 1.day.ago,
            xlsx_author: "Umzug Offerte Schweiz",
            template: 'sheets/show'
+  end
+
+  private
+
+  def received_inquiry_responces(params)
+    if params
+        ReceivedInquiryResponse.order("id DESC").where('quarter = ?', params)
+    else
+        ReceivedInquiryResponse.order("id DESC")
+    end
   end
 end
