a00aa23e7f1138bd5f9f7880466338d456e97d4b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-27 07:58:23 +0200

    Move email delivery logic from model to worker for now will refactor soon.

--- app/jobs/inquiry_delivery_worker.rb
+++ app/jobs/inquiry_delivery_worker.rb
@@ -6,15 +6,15 @@ class InquiryDeliveryWorker
              handler: Sneakers::Handlers::Maxretry,
              retry_routing_key: 'inquiries',
              arguments: { 'x-dead-letter-exchange': 'activejob-retry' },
-             timeout_job_after: 2.minutes,
+             timeout_job_after: 60.seconds,
              ack: true
 
   def work(msg)
-    inquiry = inquiry(msg)
-    inquiry_delivery = MovuTransceiver.new(inquiry)
-    inquiry_delivery.post
-    inquiry_delivery.received
-    warn '❕Transceived❕ ✅'
+    warn "❕#{msg} 🔮"
+    deliver_inquiry(msg)
+    warn '❕Transceived ✅'
+    deliver_client_email(msg)
+    warn '❕Client email sent ✅'
     ack!
   rescue
     reject!
@@ -22,9 +22,23 @@ class InquiryDeliveryWorker
 
   private
 
+  def deliver_inquiry(msg)
+    inquiry = inquiry(msg)
+    MovuTransceiver.new(inquiry).preform
+  end
+
+  def deliver_client_email(msg)
+    response = inquiry(msg).received_inquiry_response
+    ClientMailer.client(
+      response.response_body.transform_keys!(&:to_sym),
+      inquiry(msg)
+    ).deliver
+  end
+
   def inquiry(msg)
-    message = JSON.parse(msg)
-    klass = message['inquiry_name'].classify.safe_constantize
-    klass.find(message['inquiry_id'])
+    JSON.parse(msg)['inquiry_name']
+      .classify
+      .safe_constantize
+      .find(JSON.parse(msg)['inquiry_id'])
   end
 end
