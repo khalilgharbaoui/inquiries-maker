68e06f8500b5a824701af8a193f8ac3db7acc97b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-28 09:43:11 +0200

    Remove queue watching task from capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -199,12 +199,6 @@ namespace :broker do
       sudo! :service, 'rabbitmq-server status | cat'
     end
   end
-  desc 'RabbitMQ queue overview...'
-  task :queues do
-    on roles(:app), in: :groups, limit: 3, wait: 11 do
-      sudo! :watch, 'rabbitmqadmin list queues name messages messages_ready messages_unacknowledged'
-    end
-  end
 end
 
 namespace :services do
