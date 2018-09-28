b7a2e8da86053e72a692cd478a7b5952a1ca385a
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-28 09:47:29 +0200

    Refactor and dry out the Movu Transciever service object class
    Added more service objects each with its own single responsibility.
    HttpPostRequester: taks a json and a url as arguments and returns a response object.
    HttpStatusCodeLogger: takes a status code string or integer and returns a warning
    ResponseLogger: takes an Inquiry object and a response object and returns pretty details.
    MovuJsonBuilder: build json out of an inquiry object specific for movu.
    MovuTransiever: sends an inquiry request and recieves a response that is saved.
    
    During this refactoring session my focus was mainly on reducing the amount of code in
    the MovuTransiever, this is way cleaner now. but im not done yet.

--- app/services/movu_transceiver.rb
+++ app/services/movu_transceiver.rb
@@ -4,90 +4,27 @@ class MovuTransceiver
   def initialize(inquiry)
     @inquiry = inquiry
     @response = nil
+    @api_url = Rails.application.credentials.dig(Rails.env.to_sym, :partner_url) +
+               Rails.application.credentials.dig(Rails.env.to_sym, :partner_api_path)
   end
 
   def preform
-    post
-    received
-  end
-
-  def post
-    exclusions = %i[id created_at updated_at origin hostname path referrer ip]
-    credentials = {
-      origin: 'thirdparty_partner',
-      partner_token: Rails.application.credentials.dig(Rails.env.to_sym, :partner_token)
-    }
-    utm_params = {
-      utm_campaign: 'umzug-offerte-schweiz',
-      utm_term: 'umzug-offerte-schweiz',
-      utm_content: 'umzug-offerte-schweiz'
-    }
-    inclusions = credentials.merge(utm_params)
-    inquiry_json = @inquiry
-                   .as_json(except: exclusions)
-                   .merge(inclusions)
-                   .to_json
-    @response = create_http_object(inquiry_json)
-    statistics(@response, @inquiry)
-  end
-
-  def received
-    # if response header is 200 meaning the server on the other side received my lead successfully. go ahead and save the response in my database. and with an association to the lead it self.
-    if @response.code == '200'
-      # TODO: ResponseFilter should filter and create object like:
-      # ReceivedMovingInquiryResponse
-      # ReceivedCleaningIquiryResponse
-      # ReceivedCleaningInquiryResponse
-      # etc...
-      # But the name FILTER DOES NOT QUITE FIT.
-      # And the responsibilty is not inside the Received inquiry response itself
-      # It does too much.
-      # this feels like a duck but how to name it?
-      ReceivedInquiryResponse.create!(
-        :"#{@inquiry.class.name.underscore}_id" =>  @inquiry.id,
-        :response_body => @response.body
-      )
-      puts '✅ SENT AND RECEIVED ✅'
-    elsif @response.code == '400'
-      puts 'validation error!'
-      statistics(@response, @inquiry)
-    else
-      puts 'Exeption'
-      statistics(@response, @inquiry)
-    end
-    # statistics(@response, @inquiry)
-  end
-
-  def statistics(response, inquiry)
-    # puts "Headers: "
-    # puts JSON::PrettyPrint.prettify(response.to_json) if response
-    puts 'Status:'
-    puts 'Code: ' + response.code if response.code # => '200'
-    puts 'Message: ' + response.message if response.message # => 'OK'
-    puts 'Class Name: ' + response.class.name if response.class.name # => 'HTTPOK'
-
-    puts 'Response body: '
-    puts JSON::PrettyPrint.prettify(response.body) if response.body
-    puts '⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️'
-    puts "LEAD ID: #{inquiry.id} !!"
-    puts "LEAD ID: #{inquiry.class.name} !!"
+    send_request
   end
 
   private
 
-  def create_http_object(json)
-    partner_url = Rails.application.credentials.dig(Rails.env.to_sym, :partner_url)
-    partner_api_path = Rails.application.credentials.dig(Rails.env.to_sym, :partner_api_path)
-    constructed_url = partner_url + partner_api_path
-    uri = URI.parse(constructed_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request['accept'] = 'application/json'
-    request['content-type'] = 'application/json'
-    request.body = json
-    # puts json
-    http.request(request) # make the post
+  def send_request
+    inquiry_json = MovuJsonBuilder.new(@inquiry).call
+    @response = HttpPostRequester.new(inquiry_json, @api_url).call
+    ResponseLogger.new(@response, @inquiry).call
+    @response.code == "200" ? receive_response : TelegramNotifier.new(HttpStatusCodeLogger.new(@response.code).call)
   end
+
+  def receive_response
+      ReceivedInquiryResponse.create!(
+            :"#{@inquiry.class.name.underscore}_id" =>  @inquiry.id,
+            :response_body => @response.body
+          )
+    end
 end
