28ad7a27929103cc692df3c93736f8d6241cac7c
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-29 01:42:17 +0200

    Refactor and document Cre module
     Fetching Rails encrypted credentials with:
        `Rails.application.credentials.dig(:environment, :credential)`
     Is too long for my taste.
     Using Cre.dig is a lot shorter:
       `Cre.dig(:environment, :credential)`
     By default Rails.env finds the default environment for us when none is
     specified and it is called with the credential only:
       `Cre.dig(:password)`
     The dig method here is just an aesthetic thing to keep us in context.
     TODO: Add support for deeply nested credentials.

--- app/services/cre.rb
+++ app/services/cre.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 module Cre
-  def self.dig(symbol)
-    Rails.application.credentials.dig(Rails.env.to_sym, symbol)
+  # Fetching Rails encrypted credentials with:
+  #   `Rails.application.credentials.dig(:environment, :credential)`
+  # Is too long for my taste.
+  # Using Cre.dig is a lot shorter:
+  #   `Cre.dig(:environment, :credential)`
+  # By default Rails.env finds the default environment for us when none is
+  # specified and it is called with the credential only:
+  #   `Cre.dig(:password)`
+  # The dig method here is just an aesthetic thing to keep us in context.
+  # TODO: Add support for deeply nested credentials.
+
+  def self.dig(env = Rails.env.to_sym, credential)
+    Rails.application.credentials.dig(
+      env.to_sym,
+      credential.to_sym
+    )
   end
 end
