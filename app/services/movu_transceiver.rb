8d4b8e04c8d82ce32007a37cef250e285e371fcd
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-29 12:14:51 +0200

    Cleanup transceiver and fixed #51

--- app/services/movu_transceiver.rb
+++ app/services/movu_transceiver.rb
@@ -18,20 +18,7 @@ class MovuTransceiver
 
   def send_and_receive
     self.response = HttpRequester.post(inquiry_json, api_url)
-    # NOTE: BUG when not using this log function some jobs get "rejected"!
-    # I think it has todo with NET::HTTP. Will be trying others soon!
-    log(response) # keep this here for now!
-    response.code == "200" ? create_received : TelegramNotifier.new(log(response))
-  end
-
-  def create_received
-    ReceivedInquiryResponse.create!(
-      :"#{inquiry.class.name.underscore}_id" =>  inquiry.id,
-      :response_body => response.body
-    )
-  end
-
-  def log(response)
-    ResponseLogger.log(response)
+    response.code == '200' ? response : \
+    TelegramNotifier.new(ResponseLogger.log(response))
   end
 end
