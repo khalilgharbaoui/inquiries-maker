254ff3dbdad35a781a32efd543ba98fef4b08a51
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-23 01:23:24 +0200

    Add schema information to all models

--- app/models/received_inquiry_response.rb
+++ app/models/received_inquiry_response.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: received_inquiry_responses
+#
+#  id                  :bigint(8)        not null, primary key
+#  quarter             :string
+#  response_body       :jsonb            not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  cleaning_inquiry_id :bigint(8)
+#  combined_inquiry_id :bigint(8)
+#  moving_inquiry_id   :bigint(8)
+#
+# Indexes
+#
+#  index_received_inquiry_responses_on_cleaning_inquiry_id  (cleaning_inquiry_id)
+#  index_received_inquiry_responses_on_combined_inquiry_id  (combined_inquiry_id)
+#  index_received_inquiry_responses_on_moving_inquiry_id    (moving_inquiry_id)
+#  index_received_inquiry_responses_on_response_body        (response_body)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cleaning_inquiry_id => cleaning_inquiries.id)
+#  fk_rails_...  (combined_inquiry_id => combined_inquiries.id)
+#  fk_rails_...  (moving_inquiry_id => moving_inquiries.id)
+#
+
+
 class ReceivedInquiryResponse < ApplicationRecord
   belongs_to :moving_inquiry, optional: true
   belongs_to :cleaning_inquiry, optional: true
--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: invoices
+#
+#  id             :bigint(8)        not null, primary key
+#  quarter        :string
+#  representation :string
+#  send_count     :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 FactoryBot.define do
   factory :invoice do
     quarter { "MyString" }
