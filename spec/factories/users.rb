2d6e590802368a7f4ba7280737c5f4f0d5182a61
Khalil Gharbaoui <khalil@adcombi.com>
2019-05-26 18:41:06 +0200

    Fix FactoryBot deprication

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -24,7 +24,7 @@
 
 FactoryBot.define do
   factory :user do
-    email "testing@test.com"
-    password "123456ABCDEF"
+    email { "testing@test.com" }
+    password { "123456ABCDEF" }
   end
 end
