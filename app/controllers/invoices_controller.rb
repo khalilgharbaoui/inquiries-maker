3cf7f01cd720da48a2da2b0b508f85a055cb989e
Khalil Gharbaoui <khalil@adcombi.com>
2019-06-02 22:04:05 +0200

    hardcode the logo url for now!

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,37 +1,44 @@
+# frozen_string_literal: true
+
 class InvoicesController < ApplicationController
   before_action :authenticate_user!
   include RescheduleInvoiceDelivery
 
   def index
-    @inquiries = ReceivedInquiryResponse.order("id DESC")
+    @inquiries = ReceivedInquiryResponse.order('id DESC')
   end
+
   def show
     @inquiries = ReceivedInquiryResponse.where('quarter = ?', params[:quarter])
-
+    @logo_url = 'https://umzug-offerte-schweiz.com/wp-content/uploads/2015/12/umzug-offerte-schweiz-logo-2016.png'
     render pdf: file_name(@inquiries),
-      template: "invoices/show.pdf",
-      layout: false,
-      page_size: 'A4',
-      encoding: 'UTF-8',
-      dpi: '300',
-      # viewport_size: "1280x1024",
-      page_height: "297mm",
-      page_width: "210mm",
-      handlers: [ :slim ],
-      margin: { :left => "3mm",:right => "3mm", :top => "0mm", :bottom => "0mm" },
-      print_media_type:               false,
-      disable_smart_shrinking:        true,
-      zoom: zoom_level,
-      show_as_html: params.key?('debug') # allow debugging based on url param
+           template: 'invoices/show.pdf',
+           layout: false,
+           page_size: 'A4',
+           encoding: 'UTF-8',
+           dpi: '300',
+           # viewport_size: "1280x1024",
+           page_height: '297mm',
+           page_width: '210mm',
+           handlers: [:slim],
+           margin: { left: '3mm', right: '3mm', top: '0mm', bottom: '0mm' },
+           print_media_type: false,
+           disable_smart_shrinking: true,
+           zoom: zoom_level,
+           show_as_html: params.key?('debug'),
+           progress: proc { |output| puts output }# allow debugging based on url param
   end
 
   private
+
   def file_name(inquiries)
-    helpers.detail('uos').tr(' ','-') + '-Invoice-' + inquiries.first.quarter.tr(' ', '-')
+    helpers.detail('uos').tr(' ', '-') + '-Invoice-' + inquiries.first.quarter.tr(' ', '-')
   end
 
   def zoom_level
     # Ugly but the only workaround for now when using Alpine Linux
+    return 1.3 unless RUBY_PLATFORM.match?(/linux-musl/)
+
     RUBY_PLATFORM.match?(/linux-musl/) ? 1.3 : 1
   end
 end
