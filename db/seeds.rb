173846a67f02b11f22c0c1d079f94e7274d0d893
Khalil Gharbaoui <khalil@adcombi.com>
2020-09-07 04:07:13 +0200

    Seed test user only once

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  User.create!([
-                 { email: 'test@test.com', password: '123456', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: '2018-05-13 16:13:50', last_sign_in_at: '2018-05-13 16:13:50', current_sign_in_ip: '::1', last_sign_in_ip: '::1' }
-               ])
+  unless User.find_by(email: 'test@test.com')
+    User.create!([
+                   { email: 'test@test.com', password: '123456', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: '2018-05-13 16:13:50', last_sign_in_at: '2018-05-13 16:13:50', current_sign_in_ip: '::1', last_sign_in_ip: '::1' }
+                 ])
+  end
   require 'faker'
   require 'factory_bot_rails'
   %i[moving_inquiry cleaning_inquiry combined_inquiry].each do |lead_type|
