81bd83fedfa0374ce9c39bf37fe5bd6986d89fa3
Khalil Gharbaoui <khalil@adcombi.com>
2020-10-22 00:38:28 +0200

    Unfreeze string literal

--- config/initializers/same_site_cookies.rb
+++ config/initializers/same_site_cookies.rb
@@ -1,5 +1,6 @@
-class SameSiteCookies
+# frozen_string_literal: false
 
+class SameSiteCookies
   def initialize(app)
     @app = app
   end
@@ -11,9 +12,9 @@ class SameSiteCookies
 
     if set_cookie_header && !(set_cookie_header =~ /SameSite\=/)
 
-      headers['Set-Cookie'] << ';' if !(set_cookie_header =~ /;$/)
+      headers['Set-Cookie'] << ';' unless set_cookie_header =~ /;$/
       headers['Set-Cookie'] << ' SameSite=None'
-      headers['Set-Cookie'] << '; Secure'# if env['rack.url_scheme'] == 'https';
+      headers['Set-Cookie'] << '; Secure' # if env['rack.url_scheme'] == 'https';
 
     end
 
