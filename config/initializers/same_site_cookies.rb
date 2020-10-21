b008ea3878a14c04cffcbbaf61e30301de380165
Khalil Gharbaoui <khalil@adcombi.com>
2020-10-21 23:32:30 +0200

    Add Temporary SameSite Cookies fix

---
+++ config/initializers/same_site_cookies.rb
@@ -0,0 +1,24 @@
+class SameSiteCookies
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+
+    set_cookie_header = headers['Set-Cookie']
+
+    if set_cookie_header && !(set_cookie_header =~ /SameSite\=/)
+
+      headers['Set-Cookie'] << ';' if !(set_cookie_header =~ /;$/)
+      headers['Set-Cookie'] << ' SameSite=None'
+      headers['Set-Cookie'] << '; Secure'# if env['rack.url_scheme'] == 'https';
+
+    end
+
+    [status, headers, body]
+  end
+end
+
+Rails.application.config.middleware.insert_before(ActionDispatch::Cookies, SameSiteCookies)
