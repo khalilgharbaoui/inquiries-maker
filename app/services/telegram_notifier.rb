dc3bff0f45f4cbd45c137c3d77b08cb5e5654d44
Khalil Gharbaoui <khalil@adcombi.com>
2021-05-14 04:46:31 +0200

    Mitigate telegram MAX_MESSAGE_LENGTH

--- app/services/telegram_notifier.rb
+++ app/services/telegram_notifier.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class TelegramNotifier
+  MAX_MESSAGE_LENGTH = 3754
   def initialize(object)
-    @message = "*#{object.class.name}:*\n```json\n#{JSON.pretty_generate(object.as_json)}\n```"
+    @message = "#{JSON.pretty_generate(object.as_json)}"
+    @object_class_name = "*#{object.class.name}:*\n"
     @bot = Telebot::Client.new(Cre.dig(:telegram_token))
     @uid = Cre.dig(:telegram_user_id)
     send_message
@@ -11,10 +13,15 @@ class TelegramNotifier
   private
 
   def send_message
-    @bot.send_message(
-      chat_id: @uid,
-      text: @message,
-      parse_mode: 'Markdown'
-    )
+    message_chuncks = @message.chars.each_slice(MAX_MESSAGE_LENGTH).map(&:join)
+    message_chuncks.each_with_index do |text, i|
+      message = "```json\n#{text}\n```"
+      message = message.prepend(@object_class_name) if i == 0
+      @bot.send_message(
+        chat_id: @uid,
+        text: message,
+        parse_mode: 'Markdown'
+      )
+    end
   end
 end
