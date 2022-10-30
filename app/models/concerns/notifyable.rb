ffb3e7669790bb5a8cfe5610d50a74384ab90022
Khalil Gharbaoui <kaygeee@gmail.com>
2022-10-30 21:50:22 +0100

    No more telebot!

--- app/models/concerns/notifyable.rb
+++ app/models/concerns/notifyable.rb
@@ -6,6 +6,9 @@ module Notifyable
   end
 
   def send_telegram_notification
-    Rails.env.to_sym == :production ? TelegramNotifier.new(self) : warn("ℹ️  #{self.class.name} => ##{id}")
+    puts "###################################################################"
+    warn("ℹ️  #{self.class.name} => ##{id}")
+    puts "###################################################################"
+    # Rails.env.to_sym == :production ? TelegramNotifier.new(self) : warn("ℹ️  #{self.class.name} => ##{id}")
   end
 end
